3bf16f5634154af982c8ca5fe2b1ac8274598c0e
elainedias16 <elainediaspires16@gmail.com>
2023-11-26 18:15:08 -0300

    feat: start card component

--- app/src/main/java/com/example/a2023_2_proj_final_hush_hush_app/ui/MenuActivity.kt
+++ app/src/main/java/com/example/a2023_2_proj_final_hush_hush_app/ui/MenuActivity.kt
@@ -2,7 +2,9 @@ package com.example.a2023_2_proj_final_hush_hush_app.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.example.a2023_2_proj_final_hush_hush_app.R
 import com.example.a2023_2_proj_final_hush_hush_app.databinding.ActivityMenuBinding
 import com.example.a2023_2_proj_final_hush_hush_app.databinding.ActivityRegisterBinding
 
@@ -16,6 +18,10 @@ class MenuActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 //        binding.buttonLogin.setOnClickListener(this)
 //        binding.buttonNext.setOnClickListener(this)
+
+//        val cardLayout: View = layoutInflater.inflate(R.layout.card_hush_hush, null)
+//        val containerLayout = findViewById(R.layout.activity_menu)
+//        containerLayout.addView(cardLayout)
 
     }
 
